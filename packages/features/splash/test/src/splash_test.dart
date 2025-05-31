e7f5e033bc44e47493189a40011885a94c2a0577
Mahdi <mahdiasd96@yahoo.com>
2025-05-31 07:49:48 +0330

    Issue in android project.

---
+++ packages/features/splash/test/src/splash_test.dart
@@ -0,0 +1,13 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter_test/flutter_test.dart';
+
+import '../../lib/splash.dart';
+
+void main() {
+  group('Splash', () {
+    test('can be instantiated', () {
+      expect(Splash(), isNotNull);
+    });
+  });
+}
